d12b16e69a0794685aaad151e30a2ce30d7326c0
Pablo Nogueira <pablottoni@gmail.com>
2025-02-16 19:19:08 -0400

    test: add test cookies consent

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -136,14 +136,6 @@ describe('Cypress simulator', () => {
       .and('be.visible')
   });
 
-  it('Accept cookies', () => {
-
-  });
-
-  it('Decline cookies', () => {
-
-  });
-
   it('Captcha button states', () => {
 
   });
@@ -172,3 +164,35 @@ describe('Cypress simulator', () => {
 
   });
 })
+
+describe('Cypress simulator - Cookies consent', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html?skipCaptcha=true')
+    cy.contains('button', 'Login').click()
+  });
+
+  it('consents on the cookies usage', () => {
+    cy.get('#cookieConsent')
+      .as('cookieConsentBanner')
+      .find('button:contains("Accept")')
+      .click()
+
+    cy.get('@cookieConsentBanner').should('not.be.visible')
+    cy.window()
+      .its('localStorage.cookieConsent')
+      .should('be.equal', 'accepted')
+  });
+
+  it('it declines on the cookies usage', () => {
+    cy.get('#cookieConsent')
+      .as('cookieConsentBanner')
+      .find('button:contains("Decline")')
+      .click()
+
+    cy.get('@cookieConsentBanner').should('not.be.visible')
+    cy.window()
+      .its('localStorage.cookieConsent')
+      .should('be.equal', 'declined')
+  });
+
+});
